d16e0b314f37b313629386a9027f1fa3280007c9
Mudcrab353 <lazaro_353@hotmail.com>
2024-08-29 17:53:39 +1000

    Update start.py

--- src/app/func/start.py
+++ src/app/func/start.py
@@ -50,7 +50,7 @@ def start(opts):
     -drive file={image_path},format=qcow2,id=hd0,if=none,cache=writeback \
     -device virtio-blk,drive=hd0,bootindex=0 \
     -netdev user,id=mynet,hostfwd=tcp::2222-:22 \
-    -device usb-net,netdev=net1
+    -device virtio-net-pci,netdev=net1
     -netdev user,id=net1
     -device virtio-net-pci,netdev=mynet \
     -nographic -no-reboot
